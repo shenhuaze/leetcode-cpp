c78d2e6652ad6fb198e865f2947eb28ee2c47df4
shenhuaze <shenhuaze@teddymobile.cn>
2019-06-12 22:34:47 +0800

    add TwoSum.cpp

---
+++ TwoSum/TwoSum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    map<int, int> dic;
+    vector<int> result;
+    for (int i = 0; i < nums.size(); i++) {
+        if (dic.count(nums[i])) {
+            return {dic[nums[i]], i};
+        }
+        dic[target - nums[i]] = i;
+    }
+    return {};
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    for (int i : result) {
+        cout << i << "\n";
+    }
+    return 0;
+}
