f6015be3556b73b9e8582bc84b151fb136725cc9
shenhuaze <shenhuaze@pku.edu.cn>
2019-07-05 23:23:42 +0800

    add merge_k_sorted_lists.cpp

---
+++ code/merge-k-sorted-lists/merge_k_sorted_lists.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode* MergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* dummy = new ListNode(0);
+    ListNode* cur = dummy;
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            cur->next = l1;
+            l1 = l1->next;
+        } else {
+            cur->next = l2;
+            l2 = l2->next;
+        }
+        cur = cur->next;
+    }
+    if (l1) {
+        cur->next = l1;
+    } else {
+        cur->next = l2;
+    }
+    return dummy->next;
+}
+
+ListNode* MergeKLists(vector<ListNode*>& lists) {
+    if (lists.empty()) {
+        return nullptr;
+    }
+    int n = lists.size();
+    while (n > 1) {
+        int k = (n + 1) / 2;
+        for (int i = 0; i < n / 2; i++) {
+            lists[i] = MergeTwoLists(lists[i], lists[i + k]);
+        }
+        n = k;
+    }
+    return lists[0];
+}
+
+int main() {
+    ListNode* l1 = new ListNode(1);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(5);
+    ListNode* l2 = new ListNode(1);
+    l2->next = new ListNode(3);
+    l2->next->next = new ListNode(4);
+    ListNode* l3 = new ListNode(2);
+    l3->next = new ListNode(6);
+    vector<ListNode*> lists = {l1, l2, l3};
+    ListNode* result = MergeKLists(lists);
+    while (result) {
+        cout << result->val << " -> ";
+        result = result->next;
+    }
+    return 0;
+}
