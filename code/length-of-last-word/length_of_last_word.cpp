e52235969fffc84b957ee8b2ec0b5027017c2720
shenhuaze <shenhuaze@pku.edu.cn>
2019-08-12 18:28:54 +0800

    modify length_of_last_word.cpp

--- code/length-of-last-word/length_of_last_word.cpp
+++ code/length-of-last-word/length_of_last_word.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// Solution 1
 int LengthOfLastWord(string s) {
     if (s.empty()) {
         return 0;
@@ -18,8 +19,34 @@ int LengthOfLastWord(string s) {
     return len;
 }
 
+// Solution 2
+int LengthOfLastWord2(string s) {
+    if (s.empty()) {
+        return 0;
+    }
+    int size = int(s.size());
+    int left = 0;
+    int right = size - 1;
+    while (left <= size and s[left] == ' ') {
+        left += 1;
+    }
+    while (right >= 0 and s[right] == ' ') {
+        right -= 1;
+    }
+    int len = 0;
+    for (int i = left; i <= right; i++) {
+        if (s[i] == ' ') {
+            len = 0;
+        } else {
+            len += 1;
+        }
+    }
+    return len;
+}
+
 int main() {
     string s = "Hello World";
     cout << LengthOfLastWord(s);
+    cout << LengthOfLastWord2(s);
     return 0;
 }
