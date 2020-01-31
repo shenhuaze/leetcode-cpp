5ff0df1e3a914dba4f457a2fb5cbc803a3b16456
shenhuaze <shenhuaze@pku.edu.cn>
2020-01-31 09:38:38 +0800

    add populating_next_right_pointers_in_each_node.cpp

---
+++ code/populating-next-right-pointers-in-each-node/populating_next_right_pointers_in_each_node.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+            : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+Node* Connect(Node* root) {
+    if (!root) {
+        return root;
+    }
+    if (root->left) {
+        root->left->next = root->right;
+    }
+    if (root->right) {
+        if (root->next) {
+            root->right->next = root->next->left;
+        } else {
+            root->right->next = nullptr;
+        }
+    }
+    Connect(root->left);
+    Connect(root->right);
+    return root;
+}
+
+int main() {
+    auto root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    auto root_ = Connect(root);
+    return 0;
+}
